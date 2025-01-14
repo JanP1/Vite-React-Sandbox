ba5f4bee791ba75464ca2d06a6d231386e65a9fd
Jan <jakethedogssandwitch@gmail.com>
2025-01-14 21:06:09 +0100

    Added function specifying displayed parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [isSettings, setIsSettings] = useState(false);
   
+ function settingsActivated() {
+   setIsSettings(true);
+ } 
+
 
   return (
     <div className='app-container'>
+    {!isSettings && (
+      <>
       <div className='grid-area-1'>
         <div className='to-do-container'>
 
@@ -14,12 +21,13 @@ function App() {
       <div className='grid-area-2'>
         <div className='first-block'>
 
-          <div className='panel-first'> Settings </div>
+          <div className='panel-first' onClick={() => settingsActivated()}> Settings </div>
           <div className='panel-first'> Containers </div>
           <div className='panel-first'> Notes </div>
           <div className='panel-first'> To-Do </div>
         </div>
       </div>
+    </>)}
     </div>
   )
 }
