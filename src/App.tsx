dcee6dbe29edc1e59c57c063a37e237a0d51fe6f
Jan <jakethedogssandwitch@gmail.com>
2025-01-14 17:39:08 +0100

    Created vite project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='app-container'>
+      <div className='grid-area-1'>
+        <div className='to-do-container'>
+
+        </div>
+      </div>
+      <div className='grid-area-2'>
+        <div className='first-block'>
+
+          <div className='panel-first'> Settings </div>
+          <div className='panel-first'> Containers </div>
+          <div className='panel-first'> Notes </div>
+          <div className='panel-first'> To-Do </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
