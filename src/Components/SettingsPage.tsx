ea7b8df51c7cbb16fa96c61c807511d4285c8e4d
Jan <janp1krc@gmail.com>
2025-01-29 21:04:20 +0100

    Nothing much

--- src/Components/SettingsPage.tsx
+++ src/Components/SettingsPage.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './SettingsPage.css'
 
-export const SettingsPage = (props) => {
-  const [isSettings, setIsSettings] = useState();
+export const SettingsPage = () => {
 
 
   return (
